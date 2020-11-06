ac8663857fc9c416c2c426e5b7f21d183ec03187
fernanofav <fernando_sephiroth@hotmail.com>
2020-11-05 21:32:26 -0300

    Implementação dos métodos https do desafio

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,23 +12,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  //  https://github.com/fernanofav/RocketSeat-Desafio01;
+
+  const repositorie = { id: uuid(), title, url, techs, like: [] }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+  const { like }         = repositories[repositorieIndex];
+
+  if (repositorieIndex < 0){
+     return response.status(400).json({ error: 'Project not found.'});
+  }
+
+  const repositorie = {
+     id,
+     title,
+     url,
+     techs,
+     like,
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie); 
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0){
+     return response.status(400).json({ error: 'Repositorie not found.'});
+  }
+  
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send(); 
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.put("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(project => project.id === id);
+
+  if (repositorieIndex < 0){
+    return response.status(400).json({ error: 'Repositorie not found.'});
+ }
+
+  const repositorie = repositories[repositorieIndex];
+
+  repositorie.like.push(1);
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie.like.length);
 });
 
 module.exports = app;
